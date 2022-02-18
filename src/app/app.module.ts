fa8a38a23e0a89d519d3f2b53882a89459c63fa1
RussellABH <rabh.2202@gmail.com>
2022-02-18 10:41:03 -0500

    Component Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SliderComponent } from './slider/slider.component';
+import { ColorComponent } from './color/color.component';
+import { TimerComponent } from './timer/timer.component';
+import { NameComponent } from './name/name.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    ColorComponent,
+    TimerComponent,
+    NameComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
