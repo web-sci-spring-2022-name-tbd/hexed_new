60030318cee1744b7cb58c06e2c3cbaa01b2a9eb
James Liu <liuxjames88@gmail.com>
2022-02-25 02:41:02 -0500

    timer stuff

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.css']
 })
 export class TimerComponent implements OnInit {
+  intervalID = 0;
+  message = '';
+  
+
+  @Input() seconds: number = 60;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  start() { this.countDown(); }
+  stop() {
+    this.clearTimer();
+  }
+
+  clearTimer() { clearInterval(this.intervalID); }
+
+  countDown() {
+    this.clearTimer();
+    this.intervalID = window.setInterval(() => {
+      this.seconds -= 1;
+      if (this.seconds === 0) {
+        this.message = 'times up!';
+      } else {
+        this.message = `${this.seconds} seconds left`;
+      }
+    }, 1000);
   }
 
 }
