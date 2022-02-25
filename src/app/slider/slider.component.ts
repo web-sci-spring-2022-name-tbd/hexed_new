369020a0f0ea41a0404b4af72126d065b2a11051
James Liu <liuxjames88@gmail.com>
2022-02-25 01:16:29 -0500

    inputs next to the sliders now update with the correct values

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -8,10 +8,17 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SliderComponent implements OnInit {
   @Input() c: string = "";
   @Input() color: string = "";
+  @Input() val: string = "50";
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  change(obj: SliderComponent):void {
+    let input = document.getElementById(`${obj['c']}-val`) as HTMLInputElement;
+    let slider = document.getElementById(`${obj['c']}-input`) as HTMLInputElement;
+    input.value = slider.value;
   }
 
 }
